7afb67d0e3cb0af68c31a9250980de7d876e5d32
Michelle Amina Okumu <michelleaokumu@gmail.com>
2024-03-01 09:05:13 +0300

    Completed assingmnet

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,39 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
+    """
+    badges = []
+    for name in names:
+        badge = f"Hello, my name is {name}."
+        badges.append(badge)
+    return badges
+    
+print(batch_badge_creator(["Arel", "Carol"]))
+    """
 
 def assign_rooms(names):
-    return None
+    rooms = [1, 2, 3, 4, 5, 6, 7, 8]
+    return [f"Hello, {name}! You'll be assigned to room {room}!" for name, room in zip(names, rooms)]
+"""
+    rooms = [1, 2, 3, 4, 5, 6, 7, 8]
+    assignments = []
+    for name, room in zip(names, rooms):
+            assignment = f"Hello, {name}! You'll be assigned to room {room}!"
+            assignments.append(assignment)
+    return assignments
+    
+
+print(assign_rooms(["Arel", "Carol"]))
+"""
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    
+    assignments = assign_rooms(names)
+    for assignment in assignments:
+        print(assignment)
